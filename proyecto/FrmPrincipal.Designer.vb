dbfe2134659c39fc09a5ff5105e6654cb0834988
SantesDLux <uwu.metroscat@gmail.com>
2026-05-13 09:25:00 -0600

    Proyecto restaurado

---
+++ proyecto/FrmPrincipal.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ARCHIVOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DatosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ImportarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExportarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SALIRToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BUSCARCOMToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.COMDISPONIBLESToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.LblPuertoActual = New System.Windows.Forms.Label()
+        Me.BtnAceptar = New System.Windows.Forms.Button()
+        Me.BtnLimpíar = New System.Windows.Forms.Button()
+        Me.CmbTemperatura = New System.Windows.Forms.ComboBox()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Chart1
+        '
+        ChartArea2.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea2)
+        Legend2.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend2)
+        Me.Chart1.Location = New System.Drawing.Point(39, 38)
+        Me.Chart1.Name = "Chart1"
+        Series2.ChartArea = "ChartArea1"
+        Series2.Legend = "Legend1"
+        Series2.Name = "Series1"
+        Me.Chart1.Series.Add(Series2)
+        Me.Chart1.Size = New System.Drawing.Size(470, 300)
+        Me.Chart1.TabIndex = 0
+        Me.Chart1.Text = "Chart1"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ARCHIVOToolStripMenuItem, Me.BUSCARCOMToolStripMenuItem, Me.COMDISPONIBLESToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(573, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ARCHIVOToolStripMenuItem
+        '
+        Me.ARCHIVOToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DatosToolStripMenuItem, Me.SALIRToolStripMenuItem1})
+        Me.ARCHIVOToolStripMenuItem.Name = "ARCHIVOToolStripMenuItem"
+        Me.ARCHIVOToolStripMenuItem.Size = New System.Drawing.Size(70, 20)
+        Me.ARCHIVOToolStripMenuItem.Text = "ARCHIVO"
+        '
+        'DatosToolStripMenuItem
+        '
+        Me.DatosToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ImportarToolStripMenuItem, Me.ExportarToolStripMenuItem})
+        Me.DatosToolStripMenuItem.Name = "DatosToolStripMenuItem"
+        Me.DatosToolStripMenuItem.Size = New System.Drawing.Size(104, 22)
+        Me.DatosToolStripMenuItem.Text = "Datos"
+        '
+        'ImportarToolStripMenuItem
+        '
+        Me.ImportarToolStripMenuItem.Name = "ImportarToolStripMenuItem"
+        Me.ImportarToolStripMenuItem.Size = New System.Drawing.Size(120, 22)
+        Me.ImportarToolStripMenuItem.Text = "Importar"
+        '
+        'ExportarToolStripMenuItem
+        '
+        Me.ExportarToolStripMenuItem.Name = "ExportarToolStripMenuItem"
+        Me.ExportarToolStripMenuItem.Size = New System.Drawing.Size(120, 22)
+        Me.ExportarToolStripMenuItem.Text = "Exportar"
+        '
+        'SALIRToolStripMenuItem1
+        '
+        Me.SALIRToolStripMenuItem1.Name = "SALIRToolStripMenuItem1"
+        Me.SALIRToolStripMenuItem1.Size = New System.Drawing.Size(104, 22)
+        Me.SALIRToolStripMenuItem1.Text = "SALIR"
+        '
+        'BUSCARCOMToolStripMenuItem
+        '
+        Me.BUSCARCOMToolStripMenuItem.Name = "BUSCARCOMToolStripMenuItem"
+        Me.BUSCARCOMToolStripMenuItem.Size = New System.Drawing.Size(94, 20)
+        Me.BUSCARCOMToolStripMenuItem.Text = "BUSCAR COM"
+        '
+        'COMDISPONIBLESToolStripMenuItem
+        '
+        Me.COMDISPONIBLESToolStripMenuItem.Name = "COMDISPONIBLESToolStripMenuItem"
+        Me.COMDISPONIBLESToolStripMenuItem.Size = New System.Drawing.Size(120, 20)
+        Me.COMDISPONIBLESToolStripMenuItem.Text = "COM DISPONIBLES"
+        '
+        'Timer1
+        '
+        '
+        'SerialPort1
+        '
+        '
+        'LblPuertoActual
+        '
+        Me.LblPuertoActual.AutoSize = True
+        Me.LblPuertoActual.Location = New System.Drawing.Point(215, 359)
+        Me.LblPuertoActual.Name = "LblPuertoActual"
+        Me.LblPuertoActual.Size = New System.Drawing.Size(121, 13)
+        Me.LblPuertoActual.TabIndex = 2
+        Me.LblPuertoActual.Text = "Sin puerto seleccionado"
+        '
+        'BtnAceptar
+        '
+        Me.BtnAceptar.Location = New System.Drawing.Point(39, 389)
+        Me.BtnAceptar.Name = "BtnAceptar"
+        Me.BtnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnAceptar.TabIndex = 3
+        Me.BtnAceptar.Text = "CONECTAR"
+        Me.BtnAceptar.UseVisualStyleBackColor = True
+        '
+        'BtnLimpíar
+        '
+        Me.BtnLimpíar.Location = New System.Drawing.Point(434, 389)
+        Me.BtnLimpíar.Name = "BtnLimpíar"
+        Me.BtnLimpíar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnLimpíar.TabIndex = 4
+        Me.BtnLimpíar.Text = "LIMPIAR"
+        Me.BtnLimpíar.UseVisualStyleBackColor = True
+        '
+        'CmbTemperatura
+        '
+        Me.CmbTemperatura.FormattingEnabled = True
+        Me.CmbTemperatura.Location = New System.Drawing.Point(215, 389)
+        Me.CmbTemperatura.Name = "CmbTemperatura"
+        Me.CmbTemperatura.Size = New System.Drawing.Size(121, 21)
+        Me.CmbTemperatura.TabIndex = 5
+        '
+        'FrmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(573, 450)
+        Me.Controls.Add(Me.CmbTemperatura)
+        Me.Controls.Add(Me.BtnLimpíar)
+        Me.Controls.Add(Me.BtnAceptar)
+        Me.Controls.Add(Me.LblPuertoActual)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "FrmPrincipal"
+        Me.Text = "Form1"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ARCHIVOToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BUSCARCOMToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents COMDISPONIBLESToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents LblPuertoActual As Label
+    Friend WithEvents BtnAceptar As Button
+    Friend WithEvents BtnLimpíar As Button
+    Friend WithEvents DatosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ImportarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExportarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SALIRToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents CmbTemperatura As ComboBox
+End Class
---
+++ proyecto/FrmPrincipal.vb
@@ -0,0 +1,237 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Windows.Forms.DataVisualization.Charting
+
+Public Class FrmPrincipal
+    Dim modoSimulacion As Boolean = False
+    Dim tiempo As Integer = 0
+    Dim puertoConfigurado As Boolean = False
+    Dim rnd As New Random()
+
+    Dim valoresCelsius As New List(Of Double)
+
+    Private Sub FrmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = 500
+        Chart1.Series.Clear()
+        Chart1.Series.Add("Sensor")
+
+        With Chart1.Series("Sensor")
+            .ChartType = SeriesChartType.Line
+            .BorderWidth = 2
+        End With
+
+        Chart1.ChartAreas(0).AxisX.Title = "Tiempo"
+        Chart1.ChartAreas(0).AxisY.Title = "Temperatura (C)"
+
+        LblPuertoActual.Text = "Sin puerto seleccionado"
+
+        CmbTemperatura.Items.Add("Celcius")
+        CmbTemperatura.Items.Add("Farenheit")
+        CmbTemperatura.SelectedIndex = 0
+
+    End Sub
+
+    Private Sub BUSCARCOMToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BUSCARCOMToolStripMenuItem.Click
+        If SerialPort1.IsOpen Then
+            MsgBox("Desconéctate primero antes de cambiar el puerto.", MsgBoxStyle.Exclamation)
+            Return
+        End If
+
+        Dim ventana As New FrmConfiguracion()
+        If ventana.ShowDialog() = DialogResult.OK Then
+            If Not SerialPort1.IsOpen Then
+                SerialPort1.PortName = ventana.Tag.ToString()
+                LblPuertoActual.Text = "Puerto: " & SerialPort1.PortName
+                puertoConfigurado = True
+            End If
+        End If
+    End Sub
+
+    Private Sub BtnConectar_Click(sender As Object, e As EventArgs) Handles BtnAceptar.Click
+        If SerialPort1.IsOpen Or modoSimulacion Then
+            If modoSimulacion Then
+                Timer1.Stop()
+                modoSimulacion = False
+                BtnAceptar.Text = "Conectar"
+                MsgBox("Simulación detenida.")
+            Else
+                SerialPort1.Close()
+                BtnAceptar.Text = "Conectar"
+                MsgBox("Desconectado del puerto.")
+            End If
+            Return
+        End If
+
+        If puertoConfigurado Then
+            Try
+                With SerialPort1
+                    .BaudRate = 9600
+                    .Parity = Parity.None
+                    .DataBits = 8
+                    .StopBits = StopBits.One
+                End With
+                SerialPort1.Open()
+                BtnAceptar.Text = "Desconectar"
+                MsgBox("Conectado a " & SerialPort1.PortName)
+            Catch ex As Exception
+                MsgBox("Error al conectar: " & ex.Message)
+            End Try
+        Else
+            Dim respuesta = MsgBox("No has seleccionado puerto. ¿Quieres activar el MODO SIMULACIÓN?", MsgBoxStyle.YesNo Or MsgBoxStyle.Question)
+            If respuesta = MsgBoxResult.Yes Then
+                modoSimulacion = True
+                Timer1.Start()
+                BtnAceptar.Text = "Detener Simulación"
+            End If
+        End If
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        If modoSimulacion Then Return
+
+        Try
+            Dim datoCrudo As String = SerialPort1.ReadExisting()
+            Debug.WriteLine("Recibido: " & datoCrudo)
+
+            Me.Invoke(Sub()
+                          Dim valor As Double
+                          If Double.TryParse(datoCrudo.Trim(), valor) Then
+                              GraficarPunto(valor)
+                          End If
+                      End Sub)
+        Catch ex As Exception
+            Debug.WriteLine("Error: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub GraficarPunto(valorRecibido As Double)
+
+        Dim valorFinal As Double
+
+        If CmbTemperatura.Text = "Farenheit" Then
+            valorFinal = (valorRecibido * 9 / 5) + 32
+        Else
+            valorFinal = valorRecibido
+        End If
+
+        valoresCelsius.Add(valorFinal)
+
+        Chart1.Series("Sensor").Points.AddXY(tiempo, valorFinal)
+
+        tiempo += 1
+
+        If Chart1.Series("Sensor").Points.Count > 20 Then
+            Chart1.ChartAreas(0).AxisX.Minimum = tiempo - 20
+            Chart1.ChartAreas(0).AxisX.Maximum = tiempo
+        End If
+
+    End Sub
+
+    Private Sub BtnLimpiar_Click(sender As Object, e As EventArgs) Handles BtnLimpíar.Click
+        Dim resp = MsgBox("¿Seguro que quieres limpiar la gráfica? Se perderán los datos.",
+                          MsgBoxStyle.YesNo Or MsgBoxStyle.Question)
+        If resp = MsgBoxResult.No Then Return
+
+        Chart1.Series("Sensor").Points.Clear()
+        valoresCelsius.Clear()
+        tiempo = 0
+    End Sub
+
+    Private Sub ExportarCSVToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExportarToolStripMenuItem.Click
+        If Chart1.Series("Sensor").Points.Count = 0 Then
+            MsgBox("No hay datos para exportar.", MsgBoxStyle.Exclamation)
+            Return
+        End If
+
+        Dim dlg As New SaveFileDialog With {
+            .Filter = "Archivo CSV (*.csv)|*.csv",
+            .FileName = "datos_sensor_" & Format(Now, "yyyyMMdd_HHmmss")
+        }
+
+        If dlg.ShowDialog() = DialogResult.OK Then
+            Using sw As New StreamWriter(dlg.FileName)
+                sw.WriteLine("Tiempo,Celsius")
+                For i As Integer = 0 To valoresCelsius.Count - 1
+                    sw.WriteLine(i.ToString() & "," & valoresCelsius(i).ToString())
+                Next
+            End Using
+            MsgBox("CSV exportado correctamente en:" & vbNewLine & dlg.FileName)
+        End If
+    End Sub
+
+    Private Sub ImportarCSVToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ImportarToolStripMenuItem.Click
+        Dim dlg As New OpenFileDialog With {
+            .Filter = "Archivo CSV (*.csv)|*.csv"
+        }
+
+        If dlg.ShowDialog() = DialogResult.OK Then
+            Try
+                Chart1.Series("Sensor").Points.Clear()
+                valoresCelsius.Clear()
+                tiempo = 0
+
+                Dim lineas() As String = File.ReadAllLines(dlg.FileName)
+                Dim inicio As Integer = 0
+
+                If lineas(0).ToUpper().Contains("TIEMPO") Then inicio = 1
+
+                For i As Integer = inicio To lineas.Length - 1
+                    Dim partes() As String = lineas(i).Split(",")
+                    If partes.Length >= 2 Then
+                        Dim t As Double
+                        Dim v As Double
+                        If Double.TryParse(partes(0).Trim(), t) AndAlso Double.TryParse(partes(1).Trim(), v) Then
+                            valoresCelsius.Add(v)
+                            Chart1.Series("Sensor").Points.AddXY(t, v)
+                            tiempo = CInt(t) + 1
+                        End If
+                    End If
+                Next
+
+                MsgBox("CSV importado correctamente.")
+            Catch ex As Exception
+                MsgBox("Error al importar: " & ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub FrmPrincipal_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If SerialPort1.IsOpen Then SerialPort1.Close()
+    End Sub
+
+    Private Sub SALIRToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles SALIRToolStripMenuItem1.Click
+        If SerialPort1.IsOpen Then
+            Dim resp = MsgBox("Estás conectado. ¿Seguro que quieres salir?", MsgBoxStyle.YesNo Or MsgBoxStyle.Question)
+            If resp = MsgBoxResult.No Then Return
+            SerialPort1.Close()
+        End If
+        Beep()
+        End
+    End Sub
+
+    Private Sub SerialPort1_ErrorReceived(sender As Object, e As SerialErrorReceivedEventArgs) Handles SerialPort1.ErrorReceived
+        Debug.WriteLine("Error de puerto serial: " & e.EventType.ToString())
+    End Sub
+
+    Dim angulo As Double = 0
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim baseTemp As Double = 27 + 5 * Math.Sin(angulo)
+
+        Dim ruido As Double = rnd.NextDouble() * 1.5 - 0.75
+
+        Dim valorSimulado As Double = baseTemp + ruido
+
+        GraficarPunto(Math.Round(valorSimulado, 2))
+
+        angulo += 0.2
+    End Sub
+
+    Private Sub CmbTemperatura_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CmbTemperatura.SelectedIndexChanged
+        If CmbTemperatura.Text = "Celcius" Then
+            Chart1.ChartAreas(0).AxisY.Title = "Temperatura (°C)"
+        Else
+            Chart1.ChartAreas(0).AxisY.Title = "Temperatura (°F)"
+        End If
+    End Sub
+End Class
